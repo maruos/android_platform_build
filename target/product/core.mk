01300735ce4f87075b27883d8fc6ee78fe4b4e24
Preetam D'Souza <preetamjdsouza@gmail.com>
2015-10-13 20:06:49 -0700

    maru: Remove QuickSearchBox ("Search") from AOSP builds
    This app is way too old to be included anymore; there are much better
    alternatives from the Play Store.
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>
    (cherry picked from commit 0fc60116bf1774800704a7706c427d91862e698e)

--- target/product/core.mk
+++ target/product/core.mk
@@ -1,5 +1,7 @@
 #
 # Copyright (C) 2007 The Android Open Source Project
+# Copyright (C) 2015-2016 Preetam J. D'Souza
+# Copyright (C) 2016 The Maru OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -50,7 +52,6 @@ PRODUCT_PACKAGES += \
     PrintSpooler \
     PrintRecommendationService \
     ProxyHandler \
-    QuickSearchBox \
     Settings \
     SharedStorageBackup \
     StorageManager \
